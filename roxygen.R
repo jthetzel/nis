8732fda766254e3dc454c8f5c0aa77e0074c70f4
Jeremy Hetzel <jthetzel@gmail.com>
2011-10-18 14:30:42 -0400

    Added tar and binary of built package

--- roxygen.R
+++ roxygen.R
@@ -1,6 +1,26 @@
+## roxygenize
 require(roxygen2)
 
-setwd("c:/Users/jthetzel")
-package.dir <- "Research/nis/pkg"
+setwd("c:/Users/jthetzel/Research/nis")
+package.dir <- "pkg"
 
 roxygenize(package.dir = package.dir)
+
+
+
+## Check and build source
+setwd("c:/Users/jthetzel/Research/nis/tar")
+system('"C:/Program Files/R/R-2.14.0dev/bin/x64/R" CMD check "../pkg/"')
+system('"C:/Program Files/R/R-2.14.0dev/bin/x64/R" CMD build "../pkg/" ')
+
+## Build binary
+setwd("c:/Users/jthetzel/Research/nis/binary")
+system('"C:/Program Files/R/R-2.14.0dev/bin/x64/R" CMD build "../pkg/" --binary')
+
+
+cat('"C:/Program Files/R/R-2.14.0dev/bin/x64/R" CMD build "C:/Users/jthetzel/Research/trapezoid/"')
+build <- '"C:/Program Files/R/R-2.14.0dev/bin/x64/R" CMD build "C:/Users/jthetzel/Research/trapezoid/" --binary'
+check <- '"C:/Program Files/R/R-2.14.0dev/bin/x64/R" CMD check "C:/Users/jthetzel/Research/trapezoid/"'
+rd <- '"C:/Program Files/R/R-2.14.0dev/bin/x64/R" CMD Rd2dvi "C:/Users/jthetzel/Research/trapezoid/man/trapezoid.Rd" --pdf'
+system(build)
+
